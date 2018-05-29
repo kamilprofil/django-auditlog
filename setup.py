8842e67270e4a1448c708b505a4b3576f4c99fc7
kamilprofil <38035994+kamilprofil@users.noreply.github.com>
2018-05-29 10:51:41 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='django-auditlog',
     version='0.4.5',
-    packages=['auditlog', 'auditlog.migrations', 'auditlog.management', 'auditlog.management.commands'],
+    packages=['auditlog', 'auditlog.migrations', 'auditlog.management', 'auditlog.management.commands', 'auditlog.static'],
     package_dir={'': 'src'},
     url='https://github.com/jjkester/django-auditlog',
     license='MIT',
